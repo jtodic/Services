41e995aacb5d488e1dd1fd0e965bce4fbaf2b5e1
jtodic <51692139+jtodic@users.noreply.github.com>
2022-09-24 10:01:03 +0200

    SERVICE1_URL change

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -1,11 +1,11 @@
 import requests
 import sys
 
-SERVICE1_URL = "http://localhost:8080"
+SERVICE1_URL = "http://service1:8080"
 
 #http://api.open-notify.org/astros.json
 
-message = requests.get(sys.stdin.readline()).text #input()
+message = requests.get(sys.stdin.readline()).text #sys.stdin.readline()
 data = ["md5", message]
 print (data)
 print(requests.post(SERVICE1_URL, data="\n".join(data)))
